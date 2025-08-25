a1c9e6145abc0e2a92a8e5d07ee2fc1bbbe7a400
Crozeth <rhozethcarlmuyot@gmail.com>
2025-08-25 22:13:54 +0800

    Updated Game Card

--- gameprofile/src/components/GamesCard.jsx
+++ gameprofile/src/components/GamesCard.jsx
@@ -1,18 +1,38 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './GamesCard.css';
+import React from "react";
+import PropTypes from "prop-types";
+import "./GamesCard.css";
 
 const GamesCard = ({ games }) => {
   return (
     <div className="card games-card">
-      <h3 className="section-title">
-        🎮 Games Playing <span className="count">({games.length})</span>
-      </h3>
+      <h2 className="card-title">
+        🎮 Games Played <span className="count">({games.length})</span>
+      </h2>
+
       <div className="games-list">
         {games.map((game, index) => (
           <div key={index} className="game-item">
-            <span className="game-emoji">{game.icon}</span>
-            <span className="game-name">{game.name}</span>
+            {game.cover ? (
+              <img
+                src={
+                  game.cover.startsWith("http")
+                    ? game.cover
+                    : process.env.PUBLIC_URL + "/images/" + game.cover
+                }
+                alt={game.name}
+                className="game-cover"
+              />
+            ) : (
+              <span className="game-emoji">{game.icon}</span>
+            )}
+            <div className="game-info">
+              <span className="game-name">{game.name}</span>
+              {game.hours !== undefined && (
+                <span className="game-activity">
+                  {game.hours} hour{game.hours !== 1 ? "s" : ""} played recently
+                </span>
+              )}
+            </div>
           </div>
         ))}
       </div>
@@ -24,7 +44,9 @@ GamesCard.propTypes = {
   games: PropTypes.arrayOf(
     PropTypes.shape({
       name: PropTypes.string.isRequired,
-      icon: PropTypes.string.isRequired,
+      icon: PropTypes.string,
+      cover: PropTypes.string,
+      hours: PropTypes.number,
     })
   ).isRequired,
 };
