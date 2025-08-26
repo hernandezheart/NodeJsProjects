726ac06a792eba35b71beb14bc747e6af0ebcae0
Emmanuel Corpuz <emmanuelcorpuz1216@gmail.com>
2025-08-26 19:23:16 +0800

    Declare PUBG and Valorant1 as variable

--- gameprofile/src/data/players.js
+++ gameprofile/src/data/players.js
@@ -10,6 +10,9 @@ import Marvel from "../assets/Marvel.jpg";
 import Sims from "../assets/Sims.jpg";
 import Viper from "../assets/Viper.png";
 import HartHero2 from "../assets/harthero2.png";
+//Emman Asset
+import Valorant1 from "../assets/Valorant1.jpg";
+import PUBG from "../assets/PUBG.jpg";
 
 
 
@@ -113,23 +116,24 @@ export const Players = [
   },
   {
     code: "member5",
-    name: "Emmanuel Corpuz",
+    name: "Manuel Corpuz",
     username: "emman",
-    status: "Online",
+    status: "Offline",
     bio: "Gamer, Developer, Content Creator",
-    avatarUrl: "https://placehold.co/90x90",
+    avatarUrl: "https://plus.unsplash.com/premium_photo-1661843402797-d51337c5e42e?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Z29yaWxsYXxlbnwwfHwwfHx8MA%3D%3D",
     heroImage: "https://placehold.co/300x200", // fallback
     games: [
-      { name: "Valorant",    cover: " https://wallpapers.com/images/featured/valorant-thumbnail-gtrf9127f9y5jxm3.jpg",    hours: 95  },
-      { name: "PUBG",        cover: "https://e1.pxfuel.com/desktop-wallpaper/223/615/desktop-wallpaper-gaming-pubg-mobile-thumbnail-youtube.jpg",        hours: 180 },
-      { name: "COD",         cover: "https://placehold.co/300x200?text=COD",         hours: 260 },
-      { name: "CSGO",        cover: "https://placehold.co/300x200?text=Apex",        hours: 70  },
-      { name: "Poi",        cover: "https://placehold.co/300x200?text=Halo",        hours: 25  },
+      //{ name: "Valorant",    cover: " https://wallpapers.com/images/featured/valorant-thumbnail-gtrf9127f9y5jxm3.jpg",    hours: 95  },
+      { name: "Valorant",            cover: Valorant1,     hours: 350 },
+//      { name: "PUBG",        cover: "https://e1.pxfuel.com/desktop-wallpaper/223/615/desktop-wallpaper-gaming-pubg-mobile-thumbnail-youtube.jpg",        hours: 180 },
+      { name: "PUBG",            cover: PUBG,     hours: 350 },
+      { name: "COD",         cover: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/8f8a8fd3-ddb1-4928-aff3-b2bdd840fe38/dg6jcx0-fdae5e9d-472a-4e60-bfd6-8041a4b698d8.png/v1/fill/w_1600,h_900,q_80,strp/youtube_thumbnail_call_of_duty_by_juzoyena_dg6jcx0-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9OTAwIiwicGF0aCI6IlwvZlwvOGY4YThmZDMtZGRiMS00OTI4LWFmZjMtYjJiZGQ4NDBmZTM4XC9kZzZqY3gwLWZkYWU1ZTlkLTQ3MmEtNGU2MC1iZmQ2LTgwNDFhNGI2OThkOC5wbmciLCJ3aWR0aCI6Ijw9MTYwMCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.Ez87ohUSePWp3kgrYt_dVmzAorUOMXPt3y9dtg7KJqA",         hours: 260 },
+      { name: "CSGO",        cover: "https://e1.pxfuel.com/desktop-wallpaper/937/124/desktop-wallpaper-cs-go-gaming-thumbnail.jpg",        hours: 70  },
     ],
     milestones: [
-      "Achieved clutch in Valorant",
-      "Defeated the Ender Dragon in Minecraft",
-      "Completed Elden Ring without dying",
+      "Achieved Quadro Ace",
+      "5 Winner Winner, Chicken Dinner in Single Night",
+      "Reached Grandmaster in 1 week",
     ],
   },
 ];
